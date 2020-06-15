9937124132aebf7d94baee2b735671a54dde3d0d
Chong Hon Fah <honfah@basis-ai.com>
2020-06-16 00:29:34 +0800

    Update files for churn demo

--- app.py
+++ app.py
@@ -1,44 +1,114 @@
-import base64
 import json
 from io import BytesIO
 
-import numpy as np
 import pycurl
 import streamlit as st
-from PIL import Image
+import zipcodes
 
-
-def encode_image(array, dtype=np.uint8):
-    """Encode an array to base64 encoded string or bytes.
-    Args:
-        array: numpy.array
-        dtype
-    Returns:
-        base64 encoded string
-    """
-    if array is None:
-        return None
-    return base64.b64encode(np.asarray(array, dtype=dtype)).decode("utf-8")
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 
 def main():
-    st.title("COVID-19 Chest X-ray Image Classification")
+    st.title("Churn Prediction")
 
     url = st.text_input("Input endpoint url.")
     token = st.text_input("Input token.")
-    uploaded_file = st.file_uploader("Upload an image.")
+    state = st.selectbox('State', states, index=states.index('ME'))
+    area_codes = set()
+    for r in zipcodes.filter_by(state=state):
+        area_codes.update(r['area_codes'])
+    area_codes = list(area_codes)
+    if '408' in area_codes:
+        area_code = st.selectbox('Area code',
+                                 area_codes,
+                                 index=area_codes.index('408'))
+    else:
+        area_code = st.selectbox('Area code',
+                                 area_codes,
+                                 index=0)
+    area_code = int(area_code)
+    intl_plan = st.checkbox('Intl Plan', value=True)
+    intl_plan = 1 if intl_plan else 0
 
-    if url and token and uploaded_file:
+    vmail_plan = st.checkbox('VMail Plan', value=True)
+    vmail_plan = 1 if vmail_plan else 0
+
+    vmail_message = st.number_input('VMail Message',
+                                    min_value=0,
+                                    max_value=None,
+                                    value=21,
+                                    step=1)
+    custserv_calls = st.number_input('CustServ Calls',
+                                     min_value=0,
+                                     max_value=None,
+                                     value=4,
+                                     step=1)
+    day_mins = st.number_input('Day Mins',
+                               min_value=0.0,
+                               max_value=None,
+                               value=156.5,
+                               step=0.1)
+    day_calls = st.number_input('Day Calls',
+                                min_value=0,
+                                max_value=None,
+                                value=122,
+                                step=1)
+    eve_mins = st.number_input('Eve Mins',
+                               min_value=0.0,
+                               max_value=None,
+                               value=209.2,
+                               step=0.1)
+    eve_calls = st.number_input('Eve Calls',
+                                min_value=0,
+                                max_value=None,
+                                value=125,
+                                step=1)
+    night_mins = st.number_input('Night Mins',
+                                 min_value=0.0,
+                                 max_value=None,
+                                 value=158.7,
+                                 step=0.1)
+    night_calls = st.number_input('Night Calls',
+                                  min_value=0,
+                                  max_value=None,
+                                  value=81,
+                                  step=1)
+    intl_mins = st.number_input('Intl Mins',
+                                min_value=0.0,
+                                max_value=None,
+                                value=11.1,
+                                step=0.1)
+    intl_calls = st.number_input('Intl Calls',
+                                 min_value=0,
+                                 max_value=None,
+                                 value=81,
+                                 step=3)
+
+    if url and token:
         if url[:4] == 'http' and url[4] != 's':
             url = 'https' + url[4:]
 
-        image = Image.open(uploaded_file)
-
-        img = np.asarray(image.convert("RGB"))
-        img_shape = img.shape
-        encoded_img = encode_image(img.ravel())
-        data = json.dumps({"encoded_image": encoded_img,
-                           "image_shape": img_shape}).encode('utf-8')
+        data = {
+            "State": state,
+            "Area_Code": area_code,
+            "Intl_Plan": intl_plan,
+            "VMail_Plan": vmail_plan,
+            "VMail_Message": vmail_message,
+            "CustServ_Calls": custserv_calls,
+            "Day_Mins": day_mins,
+            "Day_Calls": day_calls,
+            "Eve_Mins": eve_mins,
+            "Eve_Calls": eve_calls,
+            "Night_Mins": night_mins,
+            "Night_Calls": night_calls,
+            "Intl_Mins": intl_mins,
+            "Intl_Calls": intl_calls
+        }
+        data = json.dumps(data).encode('utf-8')
 
         headers = ['Content-Type: application/json']
         headers.append('X-Bedrock-Api-Token: ' + token)
@@ -56,10 +126,9 @@ def main():
         c.perform()
         c.close()
 
-        prob = json.loads(buffer.getvalue().decode('utf-8'))["prob"]
+        prob = json.loads(buffer.getvalue().decode('utf-8'))["churn_prob"]
 
-        st.subheader(f"Probability of having COVID-19 = {prob:.6f}")
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.subheader(f"Probability of churn = {prob:.6f}")
 
 
 if __name__ == "__main__":
